a59f6b46292cd09b62e7e39e296c0d66962d9a42
crelder <z173@freenet.de>
2017-11-06 19:31:04 +0100

    Code bereinigen

--- build_backup.py
+++ build_backup.py
@@ -3,26 +3,25 @@
 
 import os, re, time
 
+# Location of Zettelkasten-director saved in a variable "zettelkastenDirectory"
+zettelkastenDirectory = os.listdir('../Zettelkasten')  
 
-##########################
-######### INPUT ##########
-##########################
-
-zettelkastenDirectory = os.listdir('../Zettelkasten') #Link to Zettelkasten-directory 
-
+# A function that filters filenames which end in ".png" and return them in an array "filteredFilenames".
+# E.g. hidden files will not show up in the backup-file.
 def zettelFilter(allFilenames):
     filteredFilenames = []
     for fileName in sorted(allFilenames):
-        if fileName.endswith(".png") or fileName.endswith(".txt") or fileName.endswith(".pdf"):
-            filteredFilenames.append(fileName[:len(fileName)-4])
+        if fileName.endswith(".png"):
+            # :len(fileName)-4 - Save the filename without the filetype at the end.
+            filteredFilenames.append(fileName[:len(fileName)-4]) 
     return filteredFilenames 
 
+# Define Filename of the Backup-File
 filename = time.strftime("%y%m%d_%H-%M-%S", time.localtime()) + "_Backup_Zettelkasten_Metadaten.txt"
-print(filename)
 
-#backup = open('backup_metadaten.txt', 'w')
+# Create a file with the defined filename
 backup = open('Backups/' + filename, 'w')
+
+# Write a header with the actual time and then list all filenames (one in each line)
 backup.write(time.strftime("%d.%m.%Y -- %H:%M:%S", time.localtime()) + '\n' + '===============' + '\n' + '\n'.join(zettelFilter(zettelkastenDirectory)))
 backup.close()
-
-input("")
